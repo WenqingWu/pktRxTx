e770d3d6db490b350319f9feb0da274a749ac7ac
WenqingWu <wenqingwu2015@gmail.com>
2018-11-29 17:45:15 +0800

    add BENCHMARK mode

--- pktRxTx.c
+++ pktRxTx.c
@@ -1,8 +1,8 @@
 
 /**
- * (C) Copyright 2018 ICT.
- * Wenqing Wu <wuwenqing@ict.ac.cn>
- */
+* (C) Copyright 2018 ICT.
+* Wenqing Wu <wuwenqing@ict.ac.cn>
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -15,6 +15,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <errno.h>
+#include <time.h>
 #else
 #include <Windows.h>
 #include <time.h>
@@ -28,6 +29,7 @@
 
 #define ECHO_MODE       1
 #define LISTENING_MODE  2
+#define BENCHMARK_MODE  3
 
 struct iphdr iph;
 struct tcphdr tcph;
@@ -36,8 +38,9 @@ pcap_t *handle;
 char pkt[PACKET_LEN];
 char pseudo_gram[PSEUDO_SIZE];
 
+int rxtx_exit = 0;
+
 /* Expect to receive acknowlegment from client */
-char need_ack = 0;              // unused for now!!
 char insert_iphdr = 1, insert_tcphdr = 1;
 
 #ifndef WIN32
@@ -65,9 +68,9 @@ int mode = ECHO_MODE;          /* running mode, default: echo mode */
 int interval = 1;      /* interval  time between two sending action*/
 int sendnum = 3;       /* number of packets for a sending request*/
 
-/* fixed MAC address for now */
-unsigned char src_mac_addr[6] = { 0x90, 0xe2, 0xba, 0x15, 0xcd, 0xd0 };
-unsigned char dest_mac_addr[6] = { 0x10, 0x1b, 0x54, 0x84, 0x83, 0xd6 };
+					   /* fixed MAC address for now */
+unsigned char src_mac_addr[6] = { 0x80, 0xfa, 0x5b, 0x33, 0x56, 0xef };
+unsigned char dest_mac_addr[6] = { 0x00, 0x0e, 0xc6, 0xd7, 0x77, 0x7b };
 
 static void log_packets(FILE ** fp, char *fname, int *fidx, const u_char * buf,
 	int len);
@@ -90,14 +93,15 @@ void sig_handler(int signo)
 		if (rx_f)
 			fclose(rx_f);
 
+		rxtx_exit = 1;
 		exit(EXIT_SUCCESS);
 	}
-}
+	}
 
 /*
 * Generic checksum calculation function
 */
-unsigned short 
+unsigned short
 csum(unsigned short *ptr, int nbytes)
 {
 	register long sum;
@@ -146,7 +150,8 @@ log_packets(FILE ** fp, char *fname, int *fidx, const u_char * buf, int len)
 					fseek(*fp, 0x10 - pos, SEEK_CUR);
 				}
 			}
-		} else {
+		}
+		else {
 		open_file:
 			sprintf(outf, fname, (*fidx)++);
 			hdr = (struct tcphdr *) (buf + TCPHDR_OFFSET);
@@ -162,7 +167,8 @@ log_packets(FILE ** fp, char *fname, int *fidx, const u_char * buf, int len)
 				*fidx = 1;
 		}
 
-	} else {
+	}
+	else {
 		*fp = NULL;
 	}
 
@@ -174,38 +180,37 @@ log_packets(FILE ** fp, char *fname, int *fidx, const u_char * buf, int len)
 	if (*fp)
 		fflush(*fp);
 }
-
-void 
+/* *
+* @ str    : content of first "len" bytes of payload
+* @ len    : length of str
+* */
+void
 send_packet(char *str, int len)
 {
 	struct pseudo_header psh;
 	unsigned short payload_len;
 	int i = 0;
+	long ran;
 
-	pkt[PAYLOAD_OFFSET] = need_ack;
+	/* fiexed payload length for now */
+	payload_len = 65;
 
-    /* fiexed payload length for now */
-	payload_len = 20;
-#if 0
-	if (!payload_len)
-		payload_len = 20;
-	else if (payload_len + HEADER_LEN > PACKET_LEN)
-		payload_len = PACKET_LEN - HEADER_LEN;
-#endif
-
-	//Ip checksum
+	/* *
+	* Ip checksum
+	* */
 	iph.tot_len = htons(sizeof(struct iphdr) + sizeof(struct tcphdr) + payload_len);
 	iph.check = 0;      //Set to 0 before calculating checksum
 	iph.check = csum((unsigned short *)&iph, sizeof(struct iphdr));
 	//printf("IP checksum: 0x%04x\n", iph.check);
 	memcpy((pkt + IPHDR_OFFSET), (char *)&iph, sizeof(struct iphdr));
 
-	tcph.check = 0;      //Set to 0 before calculating checksum
+	tcph.check = 0;
 	tcph.seq = htonl(++tx_cnt);
-	/* Now the TCP checksum 
-     * i.  craft a pseudo header
-     * ii. calculate TCP checksum
-     * */
+	/* *
+	* Now the TCP checksum
+	* i.  craft a pseudo header
+	* ii. calculate TCP checksum
+	* */
 	psh.source_address = iph.saddr;
 	psh.dest_address = iph.daddr;
 	psh.placeholder = 0;
@@ -219,16 +224,17 @@ send_packet(char *str, int len)
 
 	tcph.check = csum((unsigned short*)pseudo_gram, psize);
 
-	//printf("TCP checksum: 0x%04x\n", tcph.check);
 	memcpy((pkt + TCPHDR_OFFSET), (char *)&tcph, sizeof(struct tcphdr));
 
-	/* rest payload */
-	memcpy((pkt + PAYLOAD_OFFSET + 1), str, len);
-	for (i = (PAYLOAD_OFFSET + 1 + len); i < (PAYLOAD_OFFSET + payload_len); i++)
+	/* set payload:
+	* payload format: 'str' 0x11 0x11 0x11 ..."
+	* */
+	memcpy((pkt + PAYLOAD_OFFSET), str, len);
+	for (i = (PAYLOAD_OFFSET + len); i < (PAYLOAD_OFFSET + payload_len); i++)
 		pkt[i] = 0x11;
 
 	/* Send down the packet */
-	if (pcap_sendpacket(handle, (u_char *)& pkt, (HEADER_LEN + payload_len)) != 0) {
+	if (pcap_sendpacket(handle, (const unsigned char *)& pkt, (HEADER_LEN + payload_len)) != 0) {
 		fprintf(stderr, "Error sending the packet: %s\n", pcap_geterr(handle));
 	}
 }
@@ -237,7 +243,7 @@ send_packet(char *str, int len)
 void *
 receive_packet(void *arg)
 #else
-int WINAPI 
+int WINAPI
 receive_packet(void *arg)
 #endif
 {
@@ -254,51 +260,51 @@ receive_packet(void *arg)
 		}
 		if (res == 0)
 			continue;
-   
+
 		/* this packet is not for me!*/
 //		if (memcmp((void *)pkt_data, (void *)src_mac_addr, 6))
 //			continue;
 #if 0
 		memcpy(rcvdata, (pkt_data + PAYLOAD_OFFSET + 1), 6);
 		rcvdata[6] = '\0';
-        printf("rcv: %s\n", rcvdata);
-        /* give a response */   
-        if(memcmp((void*)rcvdata, (void *)"ackPkt", 6))
-            send_packet("ackPkt", 6);
+		printf("rcv: %s\n", rcvdata);
+		/* give a response */
+//		if (memcmp((void*)rcvdata, (void *)"ackPkt", 6))
+//			send_packet("ackPkt", 6);
 #endif
-        /* echo mode */
-        if (mode == ECHO_MODE) {
-            u_char tmp_mac_addr[6];
-            /* exchange dest_mac_addr and src_mac_addr */
-            memcpy((void *)tmp_mac_addr, (const void *)pkt_data, 6);
-            memcpy((void *)pkt_data, (const void *)(pkt_data+6), 6);
-            memcpy((void *)(pkt_data+6), (const void *)tmp_mac_addr, 6);
-            
-            /* Send down the packet */
-            if (pcap_sendpacket(handle, pkt_data, pkthdr->len) != 0) {
-                fprintf(stderr, "Error sending the packet: %s\n", pcap_geterr(handle));
-            }
-            continue;   
-        }
-        /* listening mode */
-        rx_cnt++;
-        if(rx_cnt >= 10000) {
-            printf("10000 packets arrive.\n");
-            rx_cnt = 0;
-        }
-    }
+		/* echo mode */
+		if (mode == ECHO_MODE) {
+			u_char tmp_mac_addr[6];
+			/* exchange dest_mac_addr and src_mac_addr */
+			memcpy((void *)tmp_mac_addr, (const void *)pkt_data, 6);
+			memcpy((void *)pkt_data, (const void *)(pkt_data + 6), 6);
+			memcpy((void *)(pkt_data + 6), (const void *)tmp_mac_addr, 6);
+
+			/* Send down the packet */
+			if (pcap_sendpacket(handle, (const unsigned char *)pkt_data, pkthdr->len) != 0) {
+				fprintf(stderr, "Error sending the packet: %s\n", pcap_geterr(handle));
+			}
+			continue;
+		}
+		/* listening mode */
+		rx_cnt++;
+		if (rx_cnt >= 10000) {
+			printf("10000 packets arrive.\n");
+			rx_cnt = 0;
+		}
+	}
 #ifdef WIN32
 	return 1;
 #endif
 }
 
 /* Initialize headers */
-static void 
+static void
 prepare_header(const char *name)
 {
 	struct ethhdr *eth = (struct ethhdr *) pkt;
-	//unsigned long src_addr;
-	//char dest_addr[16];
+	unsigned long src_addr;
+	char dest_addr[16];
 	int i;
 
 #if MAC_ADDR_FIX
@@ -312,7 +318,7 @@ prepare_header(const char *name)
 	else
 		eth->h_proto = 0x87a; // arbitary number prevent known ethertype
 
-	 /* Fill in the IP Header */
+							  /* Fill in the IP Header */
 	memset(&iph, 0, sizeof(struct iphdr));
 	iph.ihl = 5;
 	iph.version = 4;
@@ -321,18 +327,9 @@ prepare_header(const char *name)
 	iph.frag_off = 0;
 	iph.ttl = 255;
 
-#if 0  /* give a fixed IP addr for now */
-	if (get_interface_ipaddr(name, AF_INET, (unsigned char *)&src_addr, 4) > 0)
-		iph.saddr = src_addr;
-	else // get source ip failed, just fill with arbitary IP 
-#endif
-		iph.daddr = inet_addr("10.0.0.67");
-#if 0
-	if (!get_dest_ipaddr(name, dest_addr))
-		iph.daddr = inet_addr(dest_addr);
-	else //Get dest IP failed, just fill with arbitary IP
-#endif
-		iph.daddr = inet_addr("10.0.0.68");
+	/* give a fixed IP addr for now */
+	iph.daddr = inet_addr("10.0.0.67");
+	iph.daddr = inet_addr("10.0.0.68");
 
 	if (insert_tcphdr)
 		iph.protocol = IPPROTO_TCP;
@@ -346,7 +343,6 @@ prepare_header(const char *name)
 	tcph.dest = htons(50001);
 	tcph.seq = 0;
 	tcph.ack_seq = 0;
-	tcph.res1 = 0;
 	tcph.doff = 5;  //tcp header size
 	tcph.fin = 0;
 	tcph.syn = 0;
@@ -355,78 +351,66 @@ prepare_header(const char *name)
 	tcph.ack = 1;
 	tcph.urg = 0;
 	tcph.window = htons(5840); /* maximum allowed window size */
-	tcph.check = 0; //leave checksum 0 now, filled later by pseudo header
+	tcph.check = 0;             //leave checksum 0 now, filled later by pseudo header
 	tcph.urg_ptr = 0;
 	memcpy((pkt + TCPHDR_OFFSET), (char *)&tcph, sizeof(struct tcphdr));
 }
-
-#ifndef WIN32 
-/* nanoseconds for two points */
-struct 
-timespec diff(struct timespec start, struct timespec end)
-{
-	struct timespec temp;
-
-	if ((end.tv_nsec - start.tv_nsec) < 0) {
-		temp.tv_sec = end.tv_sec - start.tv_sec - 1;
-		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
-	}
-	else {
-		temp.tv_sec = end.tv_sec - start.tv_sec;
-		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
-	}
-
-	return temp;
-}
-/* delay for xx millseconds */
-void 
-time_wait(int msec) {
-	struct timespec t_s, t_e, tdiff;
-
-	if (clock_gettime(CLOCK_MONOTONIC, &t_s) < 0)
-		printf("t_s: clock_gettime failed\n");
-
-	do {
-		if (clock_gettime(CLOCK_MONOTONIC, &t_e) < 0) {
-			printf("t_s: clock_gettime failed\n");
-			exit(1);
-		}
-
-		tdiff = diff(t_s, t_e);
-		if (tdiff.tv_sec * 1000 + tdiff.tv_nsec / 1000000 > msec) { //sending interval
+/* *
+* delay for xx microseconds
+* */
+void
+time_wait(int us) {
+#ifndef WIN32
+	struct timespec t_s, t_e;
+	clock_gettime(CLOCK_REALTIME, &t_s);
+	while (!rxtx_exit) {
+		clock_gettime(CLOCK_REALTIME, &t_e);
+		if ((t_e.tv_sec - t_s.tv_sec) * 1000000 + (t_e.tv_nsec - t_s.tv_nsec) / 1000 > us)
 			break;
-		}
-	} while (1);
-}
+	}
+
 #else
-/* delay for xx millseconds */
-void 
-time_wait(int msec) {
-	clock_t tm_s, tm_e;
-	tm_s = clock();
 
-	while (1) {
-		tm_e = clock();
-		if (tm_e - tm_s >= (unsigned int)msec)
+	LARGE_INTEGER  large_interger = {0};
+	double dff;
+	long long  c1, c2;
+
+	LARGE_INTEGER m_liPerfFreq = {0};
+	if (!QueryPerformanceFrequency(&m_liPerfFreq)) {
+		printf("QueryPerformanceFrequency not support for this machine.\n");
+		return ;
+	}
+	LARGE_INTEGER m_liPerfStart = { 0 };
+	QueryPerformanceCounter(&m_liPerfStart);
+
+	LARGE_INTEGER liPerfNow = { 0 };
+
+	for (;rxtx_exit != 0;)
+	{
+		QueryPerformanceCounter(&liPerfNow);
+		double time = (((liPerfNow.QuadPart -
+			m_liPerfStart.QuadPart) * 1000000) / (double)m_liPerfFreq.QuadPart);
+		if (time >= us)
 			break;
 	}
-}
 #endif
+}
 /* print usage information of applicatioin */
-static void 
+static void
 usage(void)
 {
 	printf("\n"
 		"usage: txPckt [OPTION] [OPTION ifname]\n"
 		"\n"
 		"[OPTION]\n"
-        "\t-h : print usage information\n"
-        "\t-m <mode>: \n"
-        "\t\t1 for echo mode, send received packets back to peers.\n "
-        "\t\t2 for listening mode\n"
+		"\t-h : print usage information\n"
+		"\t-m <mode>: \n"
+		"\t\t1 for echo mode, send received packets back to peers.\n "
+		"\t\t2 for listening mode\n"
+		"\t\t3 for benchmark mode\n"
 		"\t-t <seconds>: time interval between two serial sending, default:1\n"
-	    "\t-i <interface>: network interface to send/receive packets\n "
-        "\t-n <number packets>: number of packets for a sending action, default:3\n"
+		"\t-i <interface>: network interface to send/receive packets\n "
+		"\t-n <number packets>: number of packets for a sending action, default:3\n"
 		"\t-b : log packet content to file as binary mode\n"
 		"\t-f <filenameprefix> : may include directory, write packets content\n"
 		"\t\tto ./dir/<filename>-tx-xxx.log\n\n");
@@ -435,14 +419,14 @@ usage(void)
 int get_options(int argc, char *argv[])
 {
 	int inum, i;
-    int setdev = 0;        
+	int setdev = 0;
 
 #ifndef WIN32
 	while ((i = getopt(argc, argv, "bvhf:t:n:i:m:")) != -1) {
 		switch (i) {
-        case 'm':
-            mode = atoi(optarg);
-            break;
+		case 'm':
+			mode = atoi(optarg);
+			break;
 		case 't':
 			interval = atoi(optarg);
 			break;
@@ -557,9 +541,9 @@ int get_options(int argc, char *argv[])
 	}
 #endif
 	/* if user did not give a specific adapter, list all the adapters available,
-	 * and let the user choose one to use.
-	 * */
-	if (!setdev){
+	* and let the user choose one to use.
+	* */
+	if (!setdev) {
 		pcap_if_t *alldevs;
 		pcap_if_t *d;
 
@@ -599,20 +583,8 @@ int get_options(int argc, char *argv[])
 
 		strcpy(dev, d->name);
 
-	} else {  // if (!argv[optind])
-#if 0
-		bpf_u_int32 mask;	/* subnet mask */
-		bpf_u_int32 net;	/* ip */
-
-		/* get network number and mask associated with capture device */
-		if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-			printf("Couldn't get netmask for device %s: %s\n",
-				dev, errbuf);
-			net = 0;
-			mask = 0;
-			exit(EXIT_FAILURE);
-		}
-#endif
+	}
+	else {  // if (!argv[optind])
 	}
 	return 1;
 }
@@ -620,6 +592,7 @@ int get_options(int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
+	int i;
 
 	printf("pktRxTx: build at %s, %s\n", __DATE__, __TIME__);
 #ifndef WIN32
@@ -628,10 +601,11 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 #else
-    HANDLE hThread;
+	HANDLE hThread;
 #endif
 	/* load configuration */
 	get_options(argc, argv);
+	rxtx_exit = 0;
 
 	/* Open the adapter */
 	if ((handle = pcap_open_live(dev,	/* name of the interface(device) */
@@ -642,11 +616,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Could not open %s: %s\n", dev, errbuf);
 		exit(EXIT_FAILURE);
 	}
-
-	//tx_fidx = rx_fidx = 1;
-
-	//get_interface_macaddr((const char *) dev, src_mac_addr);
-	//get_dest_macaddr((const char *) dev, dest_mac_addr);
 
 	printf("%02x:%02x:%02x:%02x:%02x:%02x ... listening on %s\n",
 		src_mac_addr[0], src_mac_addr[1], src_mac_addr[2],
@@ -659,9 +628,9 @@ int main(int argc, char *argv[])
 #ifndef WIN32
 	if (pthread_create(&rx_thread, NULL, receive_packet, NULL))
 #else
-	if ((hThread = (HANDLE)_beginthreadex(NULL, 0, (unsigned int (__stdcall *)(void *))receive_packet, NULL, 0, &rx_thread)) == 0) 
+	if ((hThread = (HANDLE)_beginthreadex(NULL, 0, (unsigned int(__stdcall *)(void *))receive_packet, NULL, 0, &rx_thread)) == 0)
 #endif
-    {
+	{
 		printf("Rx Thread creation failed\n");
 		goto outdoor;
 	}
@@ -669,37 +638,83 @@ int main(int argc, char *argv[])
 	if (signal(SIGINT, sig_handler) == SIG_ERR)
 		printf("\ncan't catch SIGINT\n");
 
-    /* listening mode 
-     * keep receiving command from user
-     * */
-    if (mode == LISTENING_MODE) {
-        char cmd[5];
-        int num;
-        
-        prepare_header((const char *)dev);
-       
-        printf("\nInput command('quit' for exit)\nFor example, 'send 5' for sending 5 packets.\n");
-        do {
-            printf("> ");
-            scanf("%s", cmd);
-		    if (!memcmp((void *)cmd, (void *)"quit", 4))
-                goto outdoor;
+	/* *
+	* listening mode
+	* keep receiving command from user
+	* */
+	if (mode == LISTENING_MODE) {
+		char cmd[8];
+		int num;
 
-            scanf("%d", &num);
+		prepare_header((const char *)dev);
 
-            while (num--) {
-                send_packet("normal", 6);
-                time_wait(5);           // wait for 5 millseconds 
-            }
+		printf("\nInput command('quit' for exit)\nFor example, 'send 5' for sending 5 packets.\n");
+		do {
+			printf("> ");
+			scanf("%s", cmd);
+			if (!memcmp((void *)cmd, (void *)"quit", 4))
+				goto outdoor;
 
-        } while (1);
-    
-    }
+			scanf("%d", &num);
+
+			while (num--) {
+				send_packet("normal", 6);
+				time_wait(200);           // wait for 200 microseconds 
+			}
+
+		} while (!rxtx_exit);
+	}
+	/* *
+	* benchmark mode
+	* keep receiving command from user
+	* */
+	if (mode == BENCHMARK_MODE) {
+		char cmd[8];
+		int time;
+
+		prepare_header((const char *)dev);
+
+		printf("\nInput command('quit' for exit)\nFor example, 'test 60' means benchmarking for 60 seconds.\n");
+		do {
+			printf("> ");
+			scanf("%s", cmd);
+			if (!memcmp((void *)cmd, (void *)"quit", 4))
+				goto outdoor;
+
+			scanf("%d", &time);
+
+#ifndef WIN32
+			struct timespec t_s, t_e;
+			clock_gettime(CLOCK_REALTIME, &t_s);
+			while (!rxtx_exit) {
+				clock_gettime(CLOCK_REALTIME, &t_e);
+				if ((t_e.tv_sec - t_s.tv_sec) * 1000000 + (t_e.tv_nsec - t_s.tv_nsec) / 1000 >= time * 1000000)
+					break;
+
+				send_packet("normal", 6);
+				time_wait(50);           // wait for 50 microseconds 
+			}
+
+#else
+			clock_t t1, t2;
+			t1 = clock();
+			while (!rxtx_exit) {
+				t2 = clock();
+				if (t2 - t1 > time * 1000)
+					break;
+				send_packet("normal", 6);
+				time_wait(50);
+
+			}
+
+#endif
+		} while (!rxtx_exit);
+	}
 
 #ifndef WIN32    
-    pthread_join(rx_thread, NULL);
+	pthread_join(rx_thread, NULL);
 #else
-    WaitForSingleObject(hThread, INFINITE);
+	WaitForSingleObject(hThread, INFINITE);
 #endif    
 
 outdoor:
